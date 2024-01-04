fdba2d5e84e6c42a23592228228d76815f832f42
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2024-01-04 17:34:51 -0600

    Documentation update

--- research-hub/deploy.ps1
+++ research-hub/deploy.ps1
@@ -1,3 +1,24 @@
+<#
+.SYNOPSIS
+    Deploy the Research Hub resources to the target subscription.
+
+.DESCRIPTION
+    Deploy the Research Hub resources to the target subscription.
+
+.PARAMETER TargetSubscriptionId
+    The subscription ID to deploy the resources to. The subscription must already exist.
+
+.PARAMETER Location
+    The Azure region to deploy the resources to.
+
+.PARAMETER TemplateParameterFile
+    The path to the template parameter file in bicepparam format.
+
+.EXAMPLE
+    .\deploy.ps1 -TargetSubscriptionId '00000000-0000-0000-0000-000000000000' -Location 'eastus' -TemplateParameterFile '.\main.hub.bicepparam'
+#>
+
+# LATER: Be more specific about the required modules; it will speed up the initial call
 #Requires -Modules "Az"
 #Requires -PSEdition Core
 
