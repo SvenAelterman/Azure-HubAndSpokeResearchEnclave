ba22bdf43f282a9243d0bd782247bc4935b584a2
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2024-05-01 08:07:25 -0500

    Add azcopy install script Organize AIB scripts in Windows subfolder

---
+++ scripts/PowerShell/Scripts/AIB/Windows/Install-AzCopy.ps1
@@ -0,0 +1,21 @@
+function Write-Log {
+    param (
+        $Message
+    )
+
+    $FormattedTime = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
+    Write-Host "$FormattedTime Install-AzCopy: $Message"
+}
+
+$BuildDirectory = $env:TEMP
+
+$InstallerFilename = "azcopy.zip"
+$DownloadUrl = "https://aka.ms/downloadazcopy-v10-windows"
+
+Write-Log "Downloading azcopy zip file"
+Invoke-WebRequest $DownloadUrl -OutFile $BuildDirectory\$InstallerFilename
+
+Write-Log "Extracting azcopy to C:\\"
+Expand-Archive -Path "$BuildDirectory\$InstallerFilename" -DestinationPath C:\
+
+Write-Log "azcopy script completed"
